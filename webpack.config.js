56449ebe28e2f4e7e77ba083266a653ddea65005
VictoriaSultan <86721882+VictoriaSultan@users.noreply.github.com>
2022-03-23 13:10:09 +0200

    fix webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,9 @@ module.exports = {
   },
   mode: 'development',
   devServer: {
-    contentBase: path.resolve(__dirname, "./dist"),
     open: true,
     compress: true,
-    port: 8080
+    port: 8080,
   },
   module: {
     rules: [{
